d547812e29120bbd636f135ae5b120a2208b70d2
Arseniy Egoyants <shpiyon007@gmail.com>
2025-07-14 20:14:43 +0200

    check blocking webGl

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
       ],
     },
     screenshot: "only-on-failure",
-    video: "on",
+    video: "retain-on-failure",
     trace: "on-first-retry",
     navigationTimeout: 45000,
   },
